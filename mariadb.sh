d0a1f4d3bc28fbb9b6212c2024e6eb3602c9e3ae
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2015-04-09 15:57:31 +0200

    Correct the mariadb setup

--- mariadb.sh
+++ mariadb.sh
@@ -1,15 +1,17 @@
+set -e
+
 apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install -y pwgen
 
 # Config
-export MARIADB_PASS=$(pwgen -s 12 1);
+export MARIADB_PASS=vagrantroot
 export MARIADB_VAGRANT_PASS=vagrant
 
 debconf-set-selections <<< "mariadb-server-5.5 mysql-server/root_password password $MARIADB_PASS"
 debconf-set-selections <<< "mariadb-server-5.5 mysql-server/root_password_again password $MARIADB_PASS"
 
 # Install MariaDB
-DEBIAN_FRONTEND=noninteractive apt-get install -y --force-yes --allow-unauthenticated mariadb-server mariadb-client
+DEBIAN_FRONTEND=noninteractive apt-get install -y --force-yes \
+  mariadb-server mariadb-client
 
 if [ -f /home/vagrant/.mysql-passes ]
 then
@@ -19,14 +21,13 @@ fi
 echo "root:${MARIADB_PASS}" >> /home/vagrant/.mysql-passes
 echo "vagrant:${MARIADB_VAGRANT_PASS}" >> /home/vagrant/.mysql-passes
 
-mysql -uroot -p$MARIADB_PASS -e \
-  "CREATE USER 'vagrant'@'localhost' IDENTIFIED BY '$MARIADB_VAGRANT_PASS'"
+mysql -uroot -p$MARIADB_PASS <<-EOF
+  CREATE USER 'vagrant'@'localhost' IDENTIFIED BY '$MARIADB_VAGRANT_PASS';
+  GRANT ALL ON *.* TO 'vagrant'@'localhost';
+  GRANT ALL ON *.* TO 'vagrant'@'%';
+EOF
 
 echo "MariaDB root password is in /home/vagrant/.mysql-passes"
-mysql -uroot -p$MARIADB_PASS -e \
-  "CREATE DATABASE vagrant DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;"
-mysql -uroot -p$MARIADB_PASS -e \
-  "GRANT ALL ON vagrant TO 'vagrant'@'localhost';" mysql
 
  # Configure MariaDB to listen on any address.
 sed -i -e 's/^bind-address/#bind-address/' /etc/mysql/my.cnf
@@ -35,4 +36,4 @@ sed -i -e 's/^bind-address/#bind-address/' /etc/mysql/my.cnf
 # This should make it friendlier to run on low memory servers.
 sed -i -e 's/^innodb_buffer_pool_size\s*=.*/innodb_buffer_pool_size = 128M/' /etc/mysql/my.cnf
 
-echo "Created vagrant database"
+echo "MariaDB configured"
