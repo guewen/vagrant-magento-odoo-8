a186ce0d3a8448d56cfe89426b7173c723667b1e
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2015-04-20 13:13:22 +0200

    Wrong url for buildout repo

--- odoo_bootstrap.sh
+++ odoo_bootstrap.sh
@@ -4,12 +4,12 @@ git config --global user.name "Odoo Vagrant Demo"
 
 cd /home/vagrant
 if [ -d odoo ];
-  then
-    cd odoo
-    git pull
-  else 
-    git clone https://github.com/guewen/vagrant-magento-odoo-8.git -b 8.0-new-job-runner odoo
-    cd odoo
+then
+  cd odoo
+  git pull
+else
+  git clone https://github.com/guewen/odoo-connector-magento-buildout.git -b 8.0-new-job-runner odoo
+  cd odoo
 fi
 ./bootstrap.sh
 bin/buildout
